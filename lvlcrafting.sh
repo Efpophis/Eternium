4ebf28a2807b96cf9ad2478df3b57f641be047fa
Bill Crossley <efpophis@efpophis.net>
2019-12-31 14:28:11 -0500

    testing coordinate variables
    trying out using variables to configure tap coordinates

--- lvlcrafting.sh
+++ lvlcrafting.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-# craft lvl 11 greens with water stat
+CRAFT_BTN="1413 1011"
+
+
+# craft lvl 14 greens with whatever stat
+# you set this up in the game beforehand
+
+
 while [ true ]; do
   # fill inventory
+  # this loop taps the "craft" button, then taps the screen twice more in the same place.
+  # This has the effect of skipping over the fancy animation of breaking open the crafting box
+  # and rolling the stats. Each time thru the loop creates one item.
+  #
+  # this assumes you've got a full size inventory. If not, count how many spaces and change the "56" below
+  # to however many inventory spaces you have
   for i in $(seq 1 56); do
-	  input tap 1413 1011
-	  input tap 1413 1011
-	  input tap 1413 1011
+	  input tap $CRAFT_BTN
+	  input tap $CRAFT_BTN
+	  input tap $CRAFT_BTN
   done
-  # empty inventory
+
+  # empty inventory - this closes the crafting interface and salvages all greens
+  
   # close crafting interface
   input tap 1781 60
   
